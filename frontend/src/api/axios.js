9fc71e5526357ea162143072da282a0e3963faff
kritidigital <info@kritidigital.com>
2026-04-01 17:02:51 +0530

    some changes

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,25 @@
+// import axios from "axios";
+
+// const API = axios.create({
+//   baseURL: process.env.REACT_API_URL,
+// });
+
+// // Attach token automatically
+// API.interceptors.request.use((req) => {
+//   const token = localStorage.getItem("token");
+//   if (token) {
+//     req.headers.Authorization = `Bearer ${token}`;
+//   }
+//   return req;
+// });
+
+// export default API;
+
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL:
+    import.meta.env.VITE_API_URL || "http://localhost:5000/api",
 });
 
 // Attach token automatically

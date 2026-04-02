42d21722cfb4e527df7b2735d6e3aa331cd24def
kritidigital <info@kritidigital.com>
2026-04-02 11:38:16 +0530

    uploading

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,28 +1,9 @@
-// import axios from "axios";
-
-// const API = axios.create({
-//   baseURL: process.env.REACT_API_URL,
-// });
-
-// // Attach token automatically
-// API.interceptors.request.use((req) => {
-//   const token = localStorage.getItem("token");
-//   if (token) {
-//     req.headers.Authorization = `Bearer ${token}`;
-//   }
-//   return req;
-// });
-
-// export default API;
-
 import axios from "axios";
 
 const API = axios.create({
-  baseURL:
-    import.meta.env.VITE_API_URL || "http://localhost:5000/api",
+  baseURL: process.env.REACT_API_URL,
 });
 
-// Attach token automatically
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
   if (token) {
@@ -32,3 +13,4 @@ API.interceptors.request.use((req) => {
 });
 
 export default API;
+
